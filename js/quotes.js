476e6d26e2b43552b4e9c444b35691348dacd1c7
Sujeong <nksj98@naver.com>
2023-03-02 23:49:17 +0900

    feat: random quotes

---
+++ js/quotes.js
@@ -0,0 +1,56 @@
+const quotes = [
+  {
+    quote: "언제나 현재에 집중할 수 있다면 행복할것이다.",
+    author: "파울로 코엘료",
+  },
+  {
+    quote: "피할 수 없으면 즐겨라",
+    author: "로버트 엘리엇",
+  },
+  {
+    quote:
+      "먼저 핀 꽃은 먼저 진다. 남보다 먼저 공을 세우려고 조급히 서둘 것이 아니다.",
+    author: "채근담",
+  },
+  {
+    quote:
+      "절대 어제를 후회하지 마라. 인생은 오늘의 나 안에 있고 내일은 스스로 만드는 것이다.",
+    author: "L.론허바드",
+  },
+  {
+    quote:
+      "성공의 비결은 단 한 가지, 잘할 수 있는 일에 광적으로 집중하는 것이다.",
+    author: "톰 모나건",
+  },
+  {
+    quote: "마음만을 가지고 있어서는 안된다. 반드시 실천하여야 한다.",
+    author: "이소룡",
+  },
+  {
+    quote:
+      "문제는 목적지에 얼마나 빨리 가느내가 아니라 그 목적지가 어디냐는 것이다.",
+    author: "메이벨 뉴컴버",
+  },
+  {
+    quote:
+      "우선 무엇이 되고자 하는가를 자신에게 말하라 그리고 해야 할 일을 하라.",
+    author: "에픽토테스",
+  },
+  {
+    quote:
+      "너무 소심하고 까다롭게 자신의 행동을 고민하지 말라. 모든 인생은 실험이다. 더 많이 실험할수록 더 나아진다.",
+    author: "랄프 왈도 에머슨",
+  },
+  {
+    quote: " 오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.",
+    author: "앙드레 말로",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
