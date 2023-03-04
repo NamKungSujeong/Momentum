724339a5193de621ee986aa03b7e55fd9577255a
Sujeong <nksj98@naver.com>
2023-03-04 23:59:31 +0900

    feat: delete Todo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "삭제";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
 }
 
