d684f8473e379c9f721feefead2aa1bca2ff8be5
Sujeong <nksj98@naver.com>
2023-03-05 16:29:04 +0900

    feat: delete todo from localstorage

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,26 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 function deleteTodo(e) {
   const li = e.target.parentElement;
   li.remove();
+  todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+  saveTodos();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "삭제";
   button.addEventListener("click", deleteTodo);
@@ -23,7 +34,21 @@ function handleTodoSubmit(e) {
   e.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const getTodos = localStorage.getItem(TODOS_KEY);
+
+if (getTodos !== null) {
+  const parseTodos = JSON.parse(getTodos);
+  todos = parseTodos;
+  parseTodos.forEach(paintTodo);
+}
