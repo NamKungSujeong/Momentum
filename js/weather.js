50d2fb0be27075fe9ed4a0ffcdfb5fa1284f9d9d
Sujeong <nksj98@naver.com>
2023-04-07 17:48:07 +0900

    chore

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,7 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
